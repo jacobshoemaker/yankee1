c00f46ddd66b299bf45f2dc366e582ec2c8b7a6f
jacobshoemaker <jacobshoemaker92@gmail.com>
2024-10-14 15:06:06 -0500

    Completed part one with comment documentation.

--- algo-anagrams-i/python/character_match.py
+++ algo-anagrams-i/python/character_match.py
@@ -2,10 +2,26 @@
 
 # Part 1
 def is_character_match(string1, string2):
-	# Your code here
-	pass
-
-
+	# Converting both strings into lists of lowercased characters
+	string1_list_LC = list(string1.lower())
+	string2_list_LC = list(string2.lower())
+ 
+	# Creating an empty list that will be populated with each 
+	# matching character
+	letter_match_list = []
+	
+	# Iterating through string1_list_LC and appending each character
+	# to letter_match_list
+	# that is matched in string2_list_LC
+	for letter in string1_list_LC:
+		if letter in string2_list_LC:
+			letter_match_list.append(letter)
+	# If neither string1 list or string2 list is the same
+	# as letter_match_list, return False. Else, return True
+	if ((letter_match_list != string1_list_LC) and (letter_match_list != string2_list_LC)):
+		return False
+	else:
+		return True
 # Part 2
 def anagrams_for(word, list_of_words):
 	# your code here
--- algo-anagrams-i/python/character_match_spec.py
+++ algo-anagrams-i/python/character_match_spec.py
@@ -6,12 +6,12 @@ print(is_character_match('zach', 'attack') == False)
 print(is_character_match('CharM', 'mARcH') == True)
 print(is_character_match('abcde2', 'c2abed') == True)
 
-print("This test is for the challenge quesiton")
+print("This test is for the challenge question")
 print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
 
-# Part 2
+# # Part 2
 
-list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+# list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
-print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words) == [])
+# print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
+# print(anagrams_for("apple", list_of_words) == [])
