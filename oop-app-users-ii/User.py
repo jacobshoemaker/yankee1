372a4419045b478969b9767cfb8f9e11966b4fb3
jacobshoemaker <jacobshoemaker92@gmail.com>
2024-10-16 16:39:34 -0500

    Edited __init__ in User.py to pass first test in test_User.py

---
+++ oop-app-users-ii/User.py
@@ -0,0 +1,8 @@
+# your improved User class goes here
+class User:
+    
+    def __init__(self, Name, Email, Drivers_license = None):
+        self._Name = Name
+        self._Email = Email
+        self._Drivers_license = Drivers_license
+    
