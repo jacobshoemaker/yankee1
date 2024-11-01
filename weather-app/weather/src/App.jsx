4852932165602ff1e458100132ef61fffa23def8
jacobshoemaker <jacobshoemaker92@gmail.com>
2024-11-01 09:32:31 -0500

    Added some comment documentation to explain different parts of code. Not finished documenting.

--- weather-app/weather/src/App.jsx
+++ weather-app/weather/src/App.jsx
@@ -1,12 +1,12 @@
+// Necessary imports
 import axios from 'axios';
 import './App.css'
-
-
 import SearchBar from './SearchBar';
 
 
 function App() {
 
+  // Initializing apikey variable and setting it to the value of the API Key as a string for validation
   const apikey = "b354dc157fe94899af3195009243110"
 
   // useEffect( () => {
@@ -14,12 +14,16 @@ function App() {
   // }, [userInput]
   // )
 
+  // Creating getWeatherData component as async function that takes in what user types 
   const getWeatherData = async (userInput) => {
     console.log("GetWeatherData Input: " + userInput)
+
+    // Creating response variable that is equal to the data that is grabbed from the API using the API Key and reading the user input
     const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${userInput}`)
     console.log(response)
   }
 
+  // Returning the SearchBar component and runnning getWeatherData function with SearchBar component.
   return (
     <>
       <SearchBar getWeatherData={getWeatherData}/>
--- weather-app/weather/src/SearchBar.jsx
+++ weather-app/weather/src/SearchBar.jsx
@@ -2,10 +2,13 @@ import {useState} from 'react'
 import { Button, Form } from 'react-bootstrap'
 
 
-
+// Creating SearchBar component and passing in the value returned by getWeatherData
 const SearchBar = ({getWeatherData}) => {
+    // Using a hook that takes in the userInput and creating a setUserInput function
+    // And setting the default state of the userInput to an empty string using the useState() function.
     const [userInput, setUserInput] = useState("")
     
+    // Creating a handleSubmit function that takes in the event
     const handleSubmit = (event) => {
         event.preventDefault()
         console.log(userInput)
